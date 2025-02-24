7bdbc5c440c1b91d00673ba4d2b611a10fc39626
DanielSilva07 <daniel97silva.ds@gmail.com>
2025-02-24 17:10:59 -0300

    add getter,setter, manually

--- src/main/java/com/danielsilva/imcApplication/model/ClienteModel.java
+++ src/main/java/com/danielsilva/imcApplication/model/ClienteModel.java
@@ -32,4 +32,44 @@ public class ClienteModel  {
             return imc;
         }
 
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Double getImc() {
+        return imc;
+    }
+
+    public void setImc(Double imc) {
+        this.imc = imc;
+    }
 }
