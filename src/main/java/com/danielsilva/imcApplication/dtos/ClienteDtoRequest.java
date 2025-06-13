b66e935590aa806208d9ddcb2f4b4989c5a7cf24
DanielSilva07 <daniel97silva.ds@gmail.com>
2025-06-13 13:52:09 -0300

    add redis and bigdecimal on variables

--- src/main/java/com/danielsilva/imcApplication/dtos/ClienteDtoRequest.java
+++ src/main/java/com/danielsilva/imcApplication/dtos/ClienteDtoRequest.java
@@ -6,14 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClienteDtoRequest {
                 String nome;
-                Double altura;
-                Double peso ;
+                BigDecimal altura;
+                BigDecimal peso ;
                 String email ;
 
 }
