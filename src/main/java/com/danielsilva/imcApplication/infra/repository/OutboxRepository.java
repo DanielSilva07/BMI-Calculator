743cd81869bd6293df9176f2c102afda6747fa0d
DanielSilva07 <daniel97silva.ds@gmail.com>
2025-07-04 14:20:09 -0300

    organized repo

--- src/main/java/com/danielsilva/imcApplication/infra/repository/OutboxRepository.java
+++ src/main/java/com/danielsilva/imcApplication/infra/repository/OutboxRepository.java
@@ -3,20 +3,16 @@ package com.danielsilva.imcApplication.infra.repository;
 import com.danielsilva.imcApplication.domain.Outbox;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface OutboxRepository extends JpaRepository<Outbox, UUID> {
-    
-    /**
-     * Finds all unprocessed outbox messages with pagination support.
-     *
-     * @param pageable pagination information
-     * @return a page of unprocessed outbox messages
-     */
+public interface OutboxRepository extends CrudRepository<Outbox, UUID> {
+
+    List<Outbox> findByAggregateIdOrderByCreatedAtDesc(String aggregateId);
     Page<Outbox>findByProcessedFalse(Pageable pageable);
 
 }
